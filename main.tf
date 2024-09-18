2045358ceed6e8a01e35755892c347f06a3d8923
Peter Bengtson <peter@peterbengtson.com>
2024-09-18 12:10:52 +0200

    Updated Terraform configuration to OSS 1.9.6.

--- main.tf
+++ main.tf
@@ -10,9 +10,27 @@ module "aft" {
   ct_home_region              = var.ct_home_region
   tf_backend_secondary_region = var.tf_backend_secondary_region
 
+  # Terraform configuration
+  terraform_distribution = "oss"
+  terraform_version      = "1.9.6"
+
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
-  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
-  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  account_request_repo_name                     = "${var.github_username}/AFT-account-request"
+  account_provisioning_customizations_repo_name = "${var.github_username}/AFT-account-provisioning-customizations"
+  global_customizations_repo_name               = "${var.github_username}/AFT-global-customizations"
+  account_customizations_repo_name              = "${var.github_username}/AFT-account-customizations"
+
+  # Features
+  aft_feature_delete_default_vpcs_enabled = false
+  aft_feature_cloudtrail_data_events      = false
+  aft_feature_enterprise_support          = false
+
+  # Configuration
+  aft_enable_vpc                            = true   # The default is true
+  aft_vpc_endpoints                         = false
+  backup_recovery_point_retention           = 7
+  cloudwatch_log_group_retention            = 14
+  concurrent_account_factory_actions        = 10
+  log_archive_bucket_object_expiration_days = 7
+  maximum_concurrent_customizations         = 10
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-ct_management_account_id    = ""
-log_archive_account_id      = ""
-audit_account_id            = ""
-aft_management_account_id   = ""
-github_username             = ""
-ct_home_region              = ""
-tf_backend_secondary_region = ""
+ct_management_account_id    = "111122223333"
+log_archive_account_id      = "222233334444"
+audit_account_id            = "333344445555"
+aft_management_account_id   = "444455556666"
+github_username             = "AcmeInc"
+ct_home_region              = "eu-north-1"
+tf_backend_secondary_region = "eu-central-1"
